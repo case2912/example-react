5242388b13ab6532a723d73a48bbd6ee907b0b22
case2912 <shiroyama@maia.eonet.ne.jp>
2016-06-16 10:44:15 +0900

    add highcharts

---
+++ main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SimpleGraph from './Test.jsx';
+
+ReactDOM.render(
+    <SimpleGraph />,
+    document.getElementById('container')
+);
